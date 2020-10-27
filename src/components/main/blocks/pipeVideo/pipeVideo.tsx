ad48f314e06085b5903c3e753ce7b1e26f719ba6
Donnie Kim <mobaddicts@hotmail.com>
2020-10-27 12:12:36 -0500

    pipe video rich text fix

--- src/components/main/blocks/pipeVideo/pipeVideo.tsx
+++ src/components/main/blocks/pipeVideo/pipeVideo.tsx
@@ -1,18 +1,16 @@
-import React, { useEffect, useState, createRef } from 'react'
-import { Link } from 'gatsby'
+import React from 'react'
 import styles from './pipeVideo.module.scss'
-import { Image, link } from '../../../../functions'
-import Recaptcha from 'react-google-recaptcha'
+import parse from 'html-react-parser'
 
 export default ({ block, images }) => {
   const media = images.find(image => image.relativePath === 'images/' + block.video.split('/').pop());
   return (
     <section className={styles.section}>
-      <h3>{block.heading && block.heading}</h3>
+      <h3>{block.heading && parse(block.heading)}</h3>
       <div className={styles.container}>
         {block.video &&
           <div className={styles.videoContainer}><video className={styles.video} playsInline autoPlay muted loop src={media.publicURL} /></div>}
-        <p>{block.paragraph && block.paragraph}</p>
+        <div >{block.paragraph && parse(block.paragraph)}</div>
       </div>
     </section>
   )
